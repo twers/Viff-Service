d64c5b46abb854ce08337d7a0fab0da844896aa7
Jerry <cabtravis@gmail.com>
2013-12-21 16:57:55 +0800

    [feature]Zhihao: modify job test. Remove uploaded json file in %projectDir%/uploads which is created by test.

--- test/lib/job.js
+++ test/lib/job.js
@@ -1,34 +1,55 @@
 var request = require('request');
 var fs = require('fs');
+var path = require('path');
 require('../../lib/app');
 
 describe('Jobs', function () {
+  var existingFile;
+  var uploadsPath = path.join(__dirname, "../../uploads");
   this.timeout(20000); 
- // remove uploads/configFile.json 
+  // recode existing files in uploads/configFile.json 
   before(function(done){
-    fs.unlink('../../uploads/configFile.json',function(){
+    fs.readdir(uploadsPath, function (err, fileList) {
+      existingFile = fileList;
+      done();
+    });
+  });
+
+  // remove created test uploaded files
+  after(function(done){
+    fs.readdir(uploadsPath, function (err, fileList) {
+      fileList.forEach(function(file){
+        if (existingFile.indexOf(file) == -1) {
+          fs.unlink(uploadsPath+'/'+file,function(err){
+            if (err) {
+              console.error(err);
+            }
+          });
+        }  
+      }); 
       done();
     });
   });
 
   it('should return 200 when post to /jobs ',function(done){
-    var r = request.post('http://localhost:3000/jobs', handler);
+    var r = request.post('http://localhost:3000/jobs', callback);
     var form = r.form();
     form.append('jobName', 'test job');
     form.append('configFile', fs.createReadStream(__dirname + '/configFile.json'));
 
-    function handler() {
+    function callback() {
       done();
     }
   });
 
   it('should put attached file in %PROJECT_PATH/uploads from post /job', function(done){
-    var r = request.post('http://localhost:3000/jobs', handler);
+    var r = request.post('http://localhost:3000/jobs', callback);
     var form = r.form();
     form.append('jobName', 'test job');
     form.append('configFile', fs.createReadStream(__dirname + '/configFile.json'));
 
-    function handler() {
+    // function callback(err,response,body) {
+    function callback() {
       done();
     }
   });
