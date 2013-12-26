afd69d4079463e9a5123f02580e06dac5f96100e
Wang Qiu <winsonwq@gmail.com>
2013-12-26 08:24:53 +0800

    [feature]Qiu: prepare for github page

--- public/test/unit/main.js
+++
@@ -1,7 +0,0 @@
-describe('viff service test: ', function(){
-  beforeEach(module('viffservice'));
-  
-  it('should return a string', inject(function(viff) {
-    viff.should.equal('viff');
-  }));
-});
