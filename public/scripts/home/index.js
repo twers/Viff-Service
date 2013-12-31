299606d06ea935335940fa5c371836848d011ebf
XiaoFeng Yuan <xfyuan@thoughtworks.com>
2013-12-31 15:55:38 +0800

    [feat]xiaofeng&zhaoyu: can display data from mongodb in page, no test
    - use angular http ajax to get data from db, server use Jobs api
    - move header & aside template to front-end
    - refactor the structure of home angular js

--- public/scripts/home/index.js
+++ public/scripts/home/index.js
@@ -1,27 +1,27 @@
 require('angular');
 require('angular-route');
 
+var appHome = angular.module('viffservice/home',['ngRoute']);
 
-angular
-  .module('viffservice/home', ['ngRoute'])
-  .config([
-    '$routeProvider',
-    function($routeProvider) {
-      $routeProvider
-        .when('/', {
-          controller: 'HomePageCtrl',
-          templateUrl: '/templates/home/index.html'
-        })
-        .otherwise({ redirectTo: '/' });
-    }
-  ])
-  .controller('HomePageCtrl', [
-    '$scope',
-    function($scope) {
-      $scope.benefits = [
-        'happy',
-        'allo',
-        'viva la vida'
-      ];
-    }
-  ]);
+function homeConfig($routeProvider) {
+  $routeProvider.
+    when('/', {
+      controller: HomeJobList,
+      templateUrl: '/templates/home/index.html'
+    }).
+    otherwise({
+      redirectTo: '/'
+    });
+}
+
+appHome.config(homeConfig);
+
+function HomeJobList($scope, $http) {
+  $http.get('/jobs').success(function (data) {
+    $scope.joblist = data;
+  });
+  $scope.joblistClick = function () {
+    console.log('123');
+  };
+}
+
