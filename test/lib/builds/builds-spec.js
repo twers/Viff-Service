1490512f4995e02c681a38d2bcf9c45e4406d192
kiddkai <kiddkai@gmail.com>
2014-02-13 19:05:23 +0800

    [feature]zekai: add track method to build, track the process status and save to db

--- test/lib/builds/builds-spec.js
+++ test/lib/builds/builds-spec.js
@@ -1,4 +1,9 @@
+var Ev = require('events').EventEmitter;
+var sinon = require('sinon');
+var buildCruder = require('../../../lib/builds/build-cruder');
+var Build = require('../../../lib/builds/build');
 var builds = require('../../../lib/builds/builds');
+var Job = require('../../../lib/jobs').Job;
 var jobs = require('../../../lib/jobs/app').Jobs;
 var jobCollection = require('../../../lib/database')('jobs');
 
@@ -34,4 +39,59 @@ describe('Builds', function() {
         done();
       });
   });
+
+  describe('#track', function() {
+    var currBuild, currJob, ps;
+
+    beforeEach(function() {
+      sinon.stub(buildCruder, 'findOneAndUpdate');
+      currBuild = new Build({ name: 'bleh', _id: 0 });
+      currJob = new Job({ name: 'bleh', _id: 0 });
+      ps = new Ev();
+      builds.track(currJob, currBuild, ps);
+    });
+
+    afterEach(function() {
+      buildCruder.findOneAndUpdate.restore();
+    });
+
+    it('should track the exit and error event of a running process', function() {
+      ps.listeners('exit').should.have.lengthOf(1);
+    });
+
+    it('should set the status to build when exit event emitted', function() {
+      sinon.stub(currBuild, 'set');
+      ps.emit('exit', 0);
+      currBuild.set.calledWith('status').should.be.true;
+      currBuild.set.restore();
+    });
+
+    it('should save the current exit status to database when process exit', function() {
+      ps.emit('exit', 0);
+      buildCruder.findOneAndUpdate
+                 .calledWith(0, 0, currBuild.toObject())
+                 .should.be.true;
+    });
+
+    it('should set the build status with aborted when user interrupt the process', function() {
+      sinon.stub(currBuild, 'set');
+      ps.emit('exit', null, 'SIGINT');
+      currBuild.set.calledWith('status', 'aborted').should.be.true;
+    });
+
+    it('should set the build status with success when ps exit with code 0', function() {
+      sinon.stub(currBuild, 'set');
+      ps.emit('exit', 0);
+      currBuild.set.calledWith('status', 'success').should.be.true;
+    });
+
+    it('should set the build status with failure when ps when exit code is not 0', function() {
+      sinon.stub(currBuild, 'set');
+      ps.emit('exit', 1);
+      currBuild.set.calledWith('status', 'failure').should.be.true;
+    });
+  });
+
+  
+
 });
