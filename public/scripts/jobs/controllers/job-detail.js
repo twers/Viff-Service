c1e8e823896d6e640044441c334a3be7e82b6d20
kiddkai <kiddkai@gmail.com>
2014-02-19 02:52:19 +0800

    [feature]zekai: run now only care about creating build

--- public/scripts/jobs/controllers/job-detail.js
+++ public/scripts/jobs/controllers/job-detail.js
@@ -1,28 +1,19 @@
-var through = require('event-stream').through;
-
 module.exports = [
   '$scope',
   '$routeParams',
   'Jobs',
   'Builds',
-  'SockStream',
-  function (scope, params, Jobs, Builds, SockStream) {
+  function (scope, params, Jobs, Builds) {
     var id = params.id;
 
     Jobs.id(id, function (job) {
       scope.job = job;
     });
 
+
     scope.run = function () {
       var id = scope.job._id;
-      scope.$emit('viff.build.begin.event');
-      Builds.create({jid: id}, {}, function (build) {
-        var stream = SockStream(build.link);
-        stream.pipe(through(function (data) {
-            scope.$emit("viff.console.event", data);
-          })).on('end', function () {
-            scope.$emit('viff.build.end.event');
-          });
+      Builds.create({jid: id}, {}, function () {
       });
     };
   }
