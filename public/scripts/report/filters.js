b39b5716d8f2045e90d4f0078ed54b96248aa71a
Steven Liu <steve@stevenliu.com>
2014-02-20 00:49:29 +0800

    steve: integrate viff report

---
+++ public/scripts/report/filters.js
@@ -0,0 +1,28 @@
+angular.module('viffReport')
+  .filter('imagePath', function () {
+    return function(input){
+		return encodeURIComponent(input);
+	}
+  }).filter('browserFilter', function (){
+	return function(input){
+		return "(" + input[0].toUpperCase() + ")" + input.slice(1);
+	}
+}).filter('showCases', function(){
+    return function(cases, showAll){
+        var arrayToReturn = []; 
+        if(showAll){
+        	angular.forEach(cases, function(item){
+				if (item.misMatchPercentage >= 0) {
+                	arrayToReturn.push(item);
+            	}
+        	});
+        }else{
+	       	angular.forEach(cases, function(item){
+				if (item.misMatchPercentage > 0) {
+                	arrayToReturn.push(item);
+            	}
+        	});
+        }
+        return arrayToReturn;
+    };
+});;
---
+++ public/scripts/report/services.js
@@ -0,0 +1,41 @@
+angular.module('viffReport')
+  .factory('dataService', function () {
+
+    function constructViffCase (browser, url, oneCase) {
+      var viffCase = angular.copy(oneCase);
+      viffCase.browser = browser;
+      viffCase.url = url;
+      viffCase.diffPath = viffCase.images.diff;
+      viffCase.id = 'viff' + parseInt(Math.random() * new Date().getTime());
+
+      viffCase.envs = [];
+      angular.forEach(viffCase.images, function (path, envName) {
+        if(envName != 'diff') {
+          viffCase.envs.push({
+            name: envName,
+            path: path
+          });
+        }
+      });
+      return viffCase;
+    }
+
+    return {
+      request : function (callback) {
+        $.getJSON('report.json').success(function (results) {
+          var compares = results.compares;
+          var ret = [];
+          var browsers = [];
+
+          angular.forEach(compares, function (viffCases, browser) {
+            browsers.push(browser);
+            angular.forEach(viffCases, function (viffCase, url) {
+              ret.push(constructViffCase(browser, url, viffCase));
+            });
+          });
+
+          callback && callback(ret, results.diffCount, results.caseCount, results.totalAnalysisTime, browsers);
+        });  
+      }
+    };
+  });
