040c1b0fabf2d87ea2412dcaa5a2a4ef9d96edcd
Morlay <morlay@qq.com>
2014-02-25 13:04:29 +0800

    get the taskListLength

--- public/scripts/builds/controllers/build-list.js
+++ public/scripts/builds/controllers/build-list.js
@@ -1,39 +1,30 @@
 module.exports = [
-  '$rootScope',
-  '$scope',
-  '$routeParams',
-  'Builds',
-  function (rootScope, scope, params, Builds) {
+    '$rootScope',
+    '$scope',
+    '$routeParams',
+    'Builds',
+    function (rootScope, scope, params, Builds) {
+        var id = params.id;
+        scope.builds = Builds.get(id);
 
 
+        scope.getTaskListLength = function (config) {
+            // todo
+            return config.match(new RegExp(/paths:\s\[([^\]]+)]/))[1].split(',').length;
+        };
 
-    var id = params.id;
+        scope.hasRunningHistory = function () {
+            return scope.isRunning || !!(scope.builds && scope.builds.length);
+        };
+
+        getList();
 
 
+        function getList() {
+            Builds.all({ jid: id }, function () {
+            });
+        }
 
-    scope.builds = Builds.get(id);
 
-
-    rootScope.$on('viff.build.begin.event', function () {
-      scope.isRunning = true;
-    });
-
-    rootScope.$on('viff.build.end.event', function () {
-      scope.isRunning = false;
-    });
-
-    function getList() {
-      Builds.all({ jid: id }, function () {
-      });
     }
-
-    scope.hasRunningHistory = function () {
-      return scope.isRunning || !!(scope.builds && scope.builds.length);
-    };
-
-    getList();
-
-
-
-  }
 ];
